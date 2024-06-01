42dc2275eb80f9abc16b8cf98ab174840ebbc686
Cicero Lino <cicerolinoeneto@gmail.com>
2024-06-01 16:52:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import { Card } from './components/Card'
+
+export default function App() {
+  return (
+    <Card></Card>
+  )
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+export function Card() {
+  return (
+    <div className="w-60 h-[400px] bg-slate-400 ">
+      
+    </div>
+  )
+}
